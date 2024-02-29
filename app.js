7e57923c9071e1a5567061b9298e042737233380
hammad <hammadmalik247.com>
2024-03-01 00:37:11 +0500

    ifelse

---
+++ app.js
@@ -0,0 +1,55 @@
+//  console.log('hello world');
+
+//  var num1 = 11;
+
+//  if(num1 > 10){
+//      console.log('if chal rha ha!')
+//  }else{
+//      console.log('else chal rha ha')
+//  }
+
+
+
+//  var bankBalance = +prompt('enter your bank balance');
+
+//  if(profit > 1500000){
+//      console.log('salgira mubarak ho!')
+//  }else{
+//      console.log(' bhai ammi naraz hay || rishtaydaro nay hoka day diya ha.')
+//  }
+
+
+
+ var percentage = +prompt('enter your percentage!');
+
+if (percentage > 100) {
+    console.log('bhai percent to deda sahi!');
+} 
+else if (percentage < 30) {
+    console.log('maalik thala chalao!')
+}
+ else if (percentage >= 40 && percentage <= 50) {
+    console.log('B grade aya haa maalik.')
+}
+ else if(percentage >=60 && percentage <= 80){
+    console.log('A+ grade aya ha maalik')
+}
+else {
+    console.log(percentage);
+}
+
+
+
+ var num1 = 4
+ if (num1 > 10) {
+     console.log('10 sa chota ha')
+ }
+ if (num1 > 6) {
+     console.log('5 sa bara ha')
+ }
+ if (num1 > 2) {
+     console.log('1 sa bara ha')
+ }
+  else {
+     console.log('1 sa chota ha');
+ }
